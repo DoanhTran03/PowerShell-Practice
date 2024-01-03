1734458d8bc3b2a51150f728ac429cbb08ef1219
DoanhTran03 <doanhtran2003@gmail.com>
2024-01-03 16:09:21 -0500

    Gather information from the user using type-casted variable

--- InterestCalculator.ps1
+++ InterestCalculator.ps1
@@ -1,1 +1,5 @@
-Get-ExecutionPolicy
+$SingleInterest = 0
+[ValidateRange(0, 1000)][Int]$PricipalAmount = Read-Host "Please enter the principal amount (0 - 1000) in dollar: "
+[Int]$RateOfInterest = Read-Host "Interest rate from the bank (0 - 100): "
+[Int]$Time = Read-Host "Please enter tenure (0 - 120) (in year): "
+
