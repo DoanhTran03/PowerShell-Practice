5e9dc700412ba11c9dd58fa89c1c496fef511c86
DoanhTran03 <doanhtran2003@gmail.com>
2024-01-03 16:21:43 -0500

    Add single interest equation and print output with Cyan color

--- InterestCalculator.ps1
+++ InterestCalculator.ps1
@@ -1,5 +1,6 @@
-$SingleInterest = 0
-[ValidateRange(0, 1000)][Int]$PricipalAmount = Read-Host "Please enter the principal amount (0 - 1000) in dollar: "
-[Int]$RateOfInterest = Read-Host "Interest rate from the bank (0 - 100): "
-[Int]$Time = Read-Host "Please enter tenure (0 - 120) (in year): "
+[Int]$PrincipalAmount = Read-Host "Please enter the principal amount (0 - 1000) in dollar"
+[Int]$RateOfInterest = Read-Host "Interest rate from the bank (0 - 100)"
+[Int]$Time = Read-Host "Please enter tenure (0 - 120) (in year)"
 
+[Double]$SingleInterest = ($PrincipalAmount * $RateOfInterest * $Time) / 100
+Write-Host "The single interest of $PrincipalAmount`$ in $Time years is: $SingleInterest" -ForegroundColor Cyan
